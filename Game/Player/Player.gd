17d700a2fc10427cbc68d77d5444106adbdf931d
HarperThurlow <harper.thurlow@gmail.com>
2023-11-12 17:11:03 +1000

    with rigid body

--- Game/Player/Player.gd
+++ Game/Player/Player.gd
@@ -1,24 +1,27 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 
 const SPEED = 300.0
+const JUMP_SPEED = 600
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	_fixed_process(true)
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func _fixed_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		set_linear_velocity(Vector2(-SPEED, get_linear_velocity().y))
+		get_node("AnimatedSprite2D").flip_h = false
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		set_linear_velocity(Vector2(SPEED, get_linear_velocity().y))
+		get_node("AnimatedSprite2D").flip_h = true
 
-	move_and_slide()
+
+func collision(body):
+	print("hit")
+	if body.is_in_group("platform") && get_linear_velocity().y > 0:
+		print("hit")
+		set_linear_velocity(Vector2(SPEED, get_linear_velocity().y)) 
